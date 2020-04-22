6d3851cdea27c5e80edc575c9d578c927c7253c1
Jack Harter <jack.harter@me.com>
2020-04-22 00:59:04 -0500

    initial commit

---
+++ errorflash.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This is a basic bash script.
+echo -e "\033[5;31;40;mERROR: \033[0m\033[31;40mSomething went wrong.\033[0m"
---
+++ update.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+#bash script to update debian linux
+
+sudo aptitude update -y && 
+sudo aptitude upgrade -y &&
+echo "Complete!"
